6cbff0eda34f68356df47c5e4f1f680620fdaf8a
jsantore <jsantore@bridgew.edu>
2022-09-20 11:49:46 -0400

    demo for append and remove

---
+++ listmethods.py
@@ -0,0 +1,8 @@
+courses = ["comp151", "comp152", "math130", "math120"]
+courses.append("Comp250")
+print(courses)
+courses.insert(1, "comp143")
+print(courses)
+#courses.remove("Comp250")
+courses.pop(-1)
+print(courses)
--- main.py
+++ main.py
@@ -2,3 +2,4 @@ weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday",
             "Thurbsday", "Friday", "Saturday"]
 weekdays[-3] = "Thursday"
 print(weekdays)
+print(weekdays[-8])  # list index error
