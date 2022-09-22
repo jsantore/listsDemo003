fa64cdb94df6865402df405e96760c1829504a56
jsantore <jsantore@bridgew.edu>
2022-09-22 11:53:13 -0400

    open file and split contents

--- filelist.py
+++ filelist.py
@@ -1,4 +1,9 @@
 courses_file = open("requiredCS.txt", 'r')
 lines_from_file = courses_file.readlines()
 for line in lines_from_file:
-    print(line)
+    loud_line = line.upper()
+    course_and_title = line.split(" : ")
+#    loud_line = loud_line.strip()
+    print(course_and_title[0])
+    print(course_and_title[1])
+print("those are the classes you HAVE to take for the CS Major")
